9a5cbe26b29ad35a6dc2611acdb7553dc89bcdf6
Janez <janez.cadez007@gmail.com>
2024-07-05 22:48:38 +0200

    add react fluent

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+// 👇 import the providers
+import { Providers } from "./providers";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -11,12 +14,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* 👇 Apply them to children */}
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
